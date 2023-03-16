c29adc98bec64fefbc0cf185492781b98d8b72d1
Rex <soul12358@gmail.com>
2023-03-17 00:37:56 +0800

    feat: resolve merge.

--- models/game_repository.h
+++ models/game_repository.h
@@ -15,16 +15,9 @@ public:
     GameRepository& operator=(const GameRepository& repo) = delete;
     GameRepository& operator=(GameRepository&& repo) = delete;
 
-<<<<<<< HEAD
     static GameRepository& self();
 
     std::shared_ptr<MachiKoroGame> CreateGame(std::string& id);
-=======
-    static std::shared_ptr<GameRepository> self();
-
-    std::shared_ptr<MachiKoroGame> CreateGame();
-    std::shared_ptr<MachiKoroGame> CreateGame(const std::string& id);
->>>>>>> origin/test/startGame
 
     std::shared_ptr<MachiKoroGame> FindGameByID(const std::string& id);
 
@@ -36,9 +29,6 @@ private:
     bool IsGameExist(const std::string& id);
 
 private:
-<<<<<<< HEAD
-=======
     static std::shared_ptr<GameRepository> self_;
->>>>>>> origin/test/startGame
     std::map<std::string, std::shared_ptr<MachiKoroGame>> games_;
 };
