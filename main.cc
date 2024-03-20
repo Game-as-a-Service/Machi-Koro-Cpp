5548e06b798681535f6b519e4858458f8bf8078f
Rex <soul12358@gmail.com>
2024-03-20 23:53:32 +0800

    [Fix] Fix the linking problems by modify the CMakeLists.txt
    - Change the 'Static Library' to 'Shared Library'.
    - Change the 'Header Files', 'Include Directories', and 'Linking Libraries' from PUBLIC to PRIVATE.

--- main.cc
+++ main.cc
@@ -1,29 +1,15 @@
 #include <drogon/drogon.h>
 
-#include <iostream>
-#include <string>
-#include <vector>
+int main() 
+{
+    // Set HTTP listener address and port
+    drogon::app().addListener("127.0.0.1", 8080);
 
-#include "models/machikoro_game.h"
+    // Load config file
+    // drogon::app().loadConfigFile("../config.json");
 
-int main() {
-    /*
-    //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0", 8080);
-
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-
-    //Run HTTP framework,the method will block in the internal event loop
+    // Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
 
-    */
-    std::vector<std::string> names = {"Alice", "Bob", "Cindy", "David"};
-    MachiKoroGame game(names);
-    std::cout << "Game ID: " << game.get_game_id() << std::endl;
-    std::cout << "players: " << std::endl;
-    for (const auto& player: game.get_players()) {
-        std::cout << player->get_name() << std::endl;
-    }
     return 0;
 }
